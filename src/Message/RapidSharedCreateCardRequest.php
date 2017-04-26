8928283597df93678d6006de7206e8993aaccbfc
Craig Berry <craig@redtally.com.au>
2017-04-26 12:58:17 +1000

    Make card requests extend purchase requests to reduce code duplicity

--- src/Message/RapidSharedCreateCardRequest.php
+++ src/Message/RapidSharedCreateCardRequest.php
@@ -12,7 +12,7 @@ namespace Omnipay\Eway\Message;
  *
  * @link https://eway.io/api-v3/#responsive-shared-page
  */
-class RapidSharedCreateCardRequest extends AbstractRequest
+class RapidSharedCreateCardRequest extends RapidSharedPurchaseRequest
 {
     public function getData()
     {
@@ -35,99 +35,5 @@ class RapidSharedCreateCardRequest extends AbstractRequest
         $data['Payment']['TotalAmount'] = 0;
 
         return $data;
-    }
-
-    public function sendData($data)
-    {
-        $httpResponse = $this->httpClient->post($this->getEndpoint(), null, json_encode($data))
-            ->setAuth($this->getApiKey(), $this->getPassword())
-            ->send();
-
-        return $this->response = new RapidSharedResponse($this, $httpResponse->json());
-    }
-
-    protected function getEndpoint()
-    {
-        return $this->getEndpointBase().'/CreateAccessCodeShared.json';
-    }
-
-    public function getCancelUrl()
-    {
-        return $this->getParameter('cancelUrl');
-    }
-
-    public function setCancelUrl($value)
-    {
-        return $this->setParameter('cancelUrl', $value);
-    }
-
-    public function getLogoUrl()
-    {
-        return $this->getParameter('logoUrl');
-    }
-
-    public function setLogoUrl($value)
-    {
-        return $this->setParameter('logoUrl', $value);
-    }
-
-    public function getHeaderText()
-    {
-        return $this->getParameter('headerText');
-    }
-
-    public function setHeaderText($value)
-    {
-        return $this->setParameter('headerText', $value);
-    }
-
-    public function getLanguage()
-    {
-        return $this->getParameter('language');
-    }
-
-    public function setLanguage($value)
-    {
-        return $this->setParameter('language', $value);
-    }
-
-    public function getCustomerReadOnly()
-    {
-        return $this->getParameter('customerReadOnly');
-    }
-
-    public function setCustomerReadOnly($value)
-    {
-        return $this->setParameter('customerReadOnly', $value);
-    }
-
-    public function getCustomView()
-    {
-        return $this->getParameter('customView');
-    }
-
-    public function setCustomView($value)
-    {
-        return $this->setParameter('customView', $value);
-    }
-
-    public function getVerifyCustomerPhone()
-    {
-        return $this->getParameter('verifyCustomerPhone');
-    }
-
-    public function setVerifyCustomerPhone($value)
-    {
-        return $this->setParameter('verifyCustomerPhone', $value);
-    }
-
-    public function getVerifyCustomerEmail()
-    {
-        return $this->getParameter('verifyCustomerEmail');
-    }
-
-    public function setVerifyCustomerEmail($value)
-    {
-        return $this->setParameter('verifyCustomerEmail', $value);
     }
 }
